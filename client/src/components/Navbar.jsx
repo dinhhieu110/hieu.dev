3acb15b45cdaae7f4278ca80cd1871a002c2f77f
hieu_dev <hieu.tran11002@gmail.com>
2026-09-10 22:06:02 +0700

    feat - implement navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,23 +1,40 @@
 import React, { useState } from 'react'
+import { Image } from '@imagekit/react';
 
 const Navbar = () => {
-  const [open,setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
   return (
     <div className='w-full h-16 md:h-20 flex items-center justify-between'>
       {/* LOGO */}
       <div className='flex items-center gap-4 text-2xl font-bold'>
-        <img src="/logo.png" className='w-8 h-8' alt="" />
-        <span>hieu.dev</span>
+        <Image urlEndpoint={import.meta.env.VITE_IK_URL_ENDPOINT} path="/logo.png" className='w-8 h-8' alt="" />
+        <span className='text-primary-gradient hover:text-primary-gradient-hover cursor-pointer'>&lt;hieu.dev/&gt;</span>
       </div>
       {/* Mobile menu */}
       <div className='md:hidden'>
-        <div className='cursor-pointer text-2xl' onClick={() => setOpen((prev)=> !prev)}>{
+        <div className='cursor-pointer text-2xl' onClick={() => setOpen((prev) => !prev)}>{
           open ? "X" : "☰"}
         </div>
-        <div className='w-full h-screen flex flex-col items-center justify-center'></div>
+        <div className={`w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 transition-all ease-in-out ${open ? "right-0" : "-right-[100%]"}`}>
+          <a href="/">Home</a>
+          <a href="/">Trending</a>
+          <a href="/">Most Popular</a>
+          <a href="/">About</a>
+          <a href="/">
+            <button className='py-2 px-4 rounded-3xl bg-primary-gradient hover:bg-primary-gradient-hover text-white'>Login</button>
+          </a>
+        </div>
       </div>
       {/* Desktop menu */}
-      <div className='hidden md:flex'>D</div>
+      <div className='hidden md:flex items-center gap-8 xl:gap-12 font-medium'>
+        <a href="/">Home</a>
+        <a href="/">Trending</a>
+        <a href="/">Most Popular</a>
+        <a href="/">About</a>
+        <a href="/">
+          <button className='py-2 px-4 rounded-3xl bg-primary-gradient hover:bg-primary-gradient-hover text-white'>Login</button>
+        </a>
+      </div>
 
     </div>
   )
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'primary-gradient': 'linear-gradient(135deg, #2563eb 0%, #06b6d4 100%)',
+        'primary-gradient-hover': 'linear-gradient(135deg, #1d4ed8 0%, #0891b2 100%)',
+      },
+    },
   },
   plugins: [],
 };
