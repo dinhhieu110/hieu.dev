61f8d370efa4062a607d78d02decc869c618d72e
hieu_dev <hieu.tran11002@gmail.com>
2026-09-20 21:25:33 +0700

    feat - connect server to mongodb

--- server/index.js
+++ server/index.js
@@ -3,12 +3,13 @@ import userRouter from "./routes/user.route.js"
 import postRouter from "./routes/post.route.js"
 import commentRouter from "./routes/comment.route.js"
 import connectDB from "./lib/connectDB.js"
+import dotenv from "dotenv";
 const app = express()
 
 // app.get("/test", (req, res) => {
 //   res.status(200).send("it works")
 // })
-
+dotenv.config()
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
 app.use("/comments", commentRouter);
--- server/lib/connectDB.js
+++ server/lib/connectDB.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose"
 
 const connectDB = async () => {
+  console.log("process.env.DB_PASS:", process.env.MONGO)
   try {
     await mongoose.connect(process.env.MONGO)
     console.log("Connected to Database")
